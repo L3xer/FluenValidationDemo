e4ad7047a77e562001d530af120a13106ea84322
Sanya <sanya2371@gmail.com>
2017-09-14 20:26:15 +0300

    Update page layout.

--- FluentValidationDemo/FluentValidationDemo/ViewModels/MainPageViewModel.cs
+++ FluentValidationDemo/FluentValidationDemo/ViewModels/MainPageViewModel.cs
@@ -1,10 +1,33 @@
 ﻿using Prism.Mvvm;
-using Prism.Navigation;
 
 namespace FluentValidationDemo.ViewModels
 {
     public class MainPageViewModel : BindableBase
     {
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set { SetProperty(ref _email, value); }
+        }
 
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        private string _confirmPassword;
+        public string ConfirmPassword
+        {
+            get { return _confirmPassword; }
+            set { SetProperty(ref _confirmPassword, value); }
+        }
+
+        public MainPageViewModel()
+        {
+
+        }
     }
 }
